6a440c8ba5067649460992ed6f66bce798d5ff96
Jaimin Parmar <132206640+Jaimin2687@users.noreply.github.com>
2025-10-22 11:24:44 +0530

    Add files via upload

---
+++ build/CMakeFiles/flowforge.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jaimin/workflow-engine/src/Logger.cpp" "CMakeFiles/flowforge.dir/src/Logger.cpp.o" "gcc" "CMakeFiles/flowforge.dir/src/Logger.cpp.o.d"
+  "/Users/jaimin/workflow-engine/src/PluginLoader.cpp" "CMakeFiles/flowforge.dir/src/PluginLoader.cpp.o" "gcc" "CMakeFiles/flowforge.dir/src/PluginLoader.cpp.o.d"
+  "/Users/jaimin/workflow-engine/src/RuleEngine.cpp" "CMakeFiles/flowforge.dir/src/RuleEngine.cpp.o" "gcc" "CMakeFiles/flowforge.dir/src/RuleEngine.cpp.o.d"
+  "/Users/jaimin/workflow-engine/src/Storage.cpp" "CMakeFiles/flowforge.dir/src/Storage.cpp.o" "gcc" "CMakeFiles/flowforge.dir/src/Storage.cpp.o.d"
+  "/Users/jaimin/workflow-engine/src/ThreadPool.cpp" "CMakeFiles/flowforge.dir/src/ThreadPool.cpp.o" "gcc" "CMakeFiles/flowforge.dir/src/ThreadPool.cpp.o.d"
+  "/Users/jaimin/workflow-engine/src/Workflow.cpp" "CMakeFiles/flowforge.dir/src/Workflow.cpp.o" "gcc" "CMakeFiles/flowforge.dir/src/Workflow.cpp.o.d"
+  "/Users/jaimin/workflow-engine/src/WorkflowManager.cpp" "CMakeFiles/flowforge.dir/src/WorkflowManager.cpp.o" "gcc" "CMakeFiles/flowforge.dir/src/WorkflowManager.cpp.o.d"
+  "/Users/jaimin/workflow-engine/src/main.cpp" "CMakeFiles/flowforge.dir/src/main.cpp.o" "gcc" "CMakeFiles/flowforge.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/flowforge.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flowforge.dir/src/Logger.cpp.o"
+  "CMakeFiles/flowforge.dir/src/Logger.cpp.o.d"
+  "CMakeFiles/flowforge.dir/src/PluginLoader.cpp.o"
+  "CMakeFiles/flowforge.dir/src/PluginLoader.cpp.o.d"
+  "CMakeFiles/flowforge.dir/src/RuleEngine.cpp.o"
+  "CMakeFiles/flowforge.dir/src/RuleEngine.cpp.o.d"
+  "CMakeFiles/flowforge.dir/src/Storage.cpp.o"
+  "CMakeFiles/flowforge.dir/src/Storage.cpp.o.d"
+  "CMakeFiles/flowforge.dir/src/ThreadPool.cpp.o"
+  "CMakeFiles/flowforge.dir/src/ThreadPool.cpp.o.d"
+  "CMakeFiles/flowforge.dir/src/Workflow.cpp.o"
+  "CMakeFiles/flowforge.dir/src/Workflow.cpp.o.d"
+  "CMakeFiles/flowforge.dir/src/WorkflowManager.cpp.o"
+  "CMakeFiles/flowforge.dir/src/WorkflowManager.cpp.o.d"
+  "CMakeFiles/flowforge.dir/src/main.cpp.o"
+  "CMakeFiles/flowforge.dir/src/main.cpp.o.d"
+  "flowforge"
+  "flowforge.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flowforge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
